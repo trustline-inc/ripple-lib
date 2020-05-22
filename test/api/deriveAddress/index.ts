074fe2759b1eb9abeca5c2a81cde9b91d8123e54
Elliot Lee <github.public@intelliot.com>
2020-05-22 01:27:55 -0700

    Fix linting

--- test/api/deriveAddress/index.ts
+++ test/api/deriveAddress/index.ts
@@ -7,8 +7,8 @@ import {TestSuite} from '../../utils'
  * - Check out "test/api/index.ts" for more information about the test runner.
  */
 export default <TestSuite>{
-  'returns address for public key': async (api, address) => {
-    var address = api.deriveAddress(
+  'returns address for public key': async (api, _) => {
+    const address = api.deriveAddress(
       '035332FBA71D705BD5D97014A833BE2BBB25BEFCD3506198E14AFEA241B98C2D06'
     )
     assert.equal(address, 'rLczgQHxPhWtjkaQqn3Q6UM8AbRbbRvs5K')
